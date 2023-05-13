72d785f538c54847390aa5af49b03cef2145e783
Raxxius <raxxman@gmail.com>
2023-05-13 07:48:25 +0100

    initial commit, set background and header image

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <main>
+      <div>
+        <img src='/src/assets/images/Oathsworn Logo (Screen Ready).png'></img>
+        <h1>Oathsworn Rule pop</h1>
+      </div>
+      <div>
+        Rules go here
+      </div>
+    </main>
+  )
+}
+
+export default App
